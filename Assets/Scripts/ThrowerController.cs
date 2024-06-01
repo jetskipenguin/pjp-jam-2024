3bd0d6aac054aea7a96d026685465293a5b79577
Collin Newman <collinnewman12@gmail.com>
2024-06-01 18:23:05 -0500

    Add line trajectory

--- Assets/Scripts/ThrowerController.cs
+++ Assets/Scripts/ThrowerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class ThrowerController : MonoBehaviour
 {
+    [Header("MonoBehavior References")]
+    [SerializeField, Tooltip("Reference to the TrajectoryLine script")]
+    private TrajectoryLine _trajectoryLine;
+
+    [Header("Thrower Settings")]
     [SerializeField, Tooltip("List of objects that can be thrown")]
     public List<GameObject> throwables = new List<GameObject>();
 
@@ -19,6 +24,7 @@ public class ThrowerController : MonoBehaviour
     private bool _canThrow = true;
     private List<Collider2D> _throwableColliders = new List<Collider2D>();
     private GameObject _throwableInstance;
+    private Rigidbody2D _throwableRigidbody;
 
     public void OnEnable()
     {
@@ -32,17 +38,18 @@ public class ThrowerController : MonoBehaviour
         {
             _throwableInstance.transform.position = transform.position; // TODO: add offset for player hands
             RotateThrowableOnInput();
+            _trajectoryLine.PlotTrajectoryLine(_throwableRigidbody, _throwableInstance.transform.position, throwSpeed);
         }
 
         // Throw the throwable
         if (Input.GetButtonDown("Throw") && _canThrow)
         {
             _canThrow = false;
-            Rigidbody2D rb = _throwableInstance.GetComponent<Rigidbody2D>();
+            _trajectoryLine.ClearTrajectoryLine();
 
             // Throw the throwable in the direction of the players mouse
-            rb.velocity = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized * throwSpeed;
-            Debug.Log("Applying Velocity to Throwable: " + rb.velocity);
+            _throwableRigidbody.velocity = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized * throwSpeed;
+            Debug.Log("Applying Velocity to Throwable: " + _throwableRigidbody.velocity);
 
             StartCoroutine(TurnOnColliderOnceOutsideOfThrower());
             StartCoroutine(ThrowCooldown());
@@ -106,6 +113,7 @@ public class ThrowerController : MonoBehaviour
 
         // Instantiate the throwable
         _throwableInstance = Instantiate(selectedThrowable, transform.position, Quaternion.identity);
+        _throwableRigidbody = _throwableInstance.GetComponent<Rigidbody2D>();
 
         // Check if the throwable has a collider
         _throwableColliders = new List<Collider2D>();
---
+++ Assets/Scripts/TrajectoryLine.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// A large part of the code in this script is taken from the following source: https://www.youtube.com/watch?v=RpeRnlLgmv8&ab_channel=MuddyWolf
+public class TrajectoryLine : MonoBehaviour
+{
+    [Header("Monobehavior References")]
+    [SerializeField] private ThrowerController _throwerController;
+
+    [Header("Trajectory Line Settings")]
+    [SerializeField, Tooltip("Smoothness of the line")]
+    private int _segmentCount = 100;
+    
+    [SerializeField, Tooltip("Color of the line")]
+    private Color _lineColor = Color.white;
+
+    private LineRenderer _lineRenderer;
+
+    private void Start()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+        _lineRenderer.startColor = _lineColor;
+        _lineRenderer.endColor = _lineColor;
+    }
+
+    public void PlotTrajectoryLine(Rigidbody2D rigidbody, Vector2 pos, float throwSpeed)
+    {
+        Vector2 velocity = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized * throwSpeed;
+
+        Vector2[] trajectory = CalculateTrajectoryLine(rigidbody, pos, velocity, _segmentCount);
+        
+        _lineRenderer.positionCount = trajectory.Length;
+
+        // convert Vector2 array to Vector3 array
+        Vector3[] positions = new Vector3[trajectory.Length];
+        for (int i = 0; i < trajectory.Length; i++)
+        {
+            positions[i] = trajectory[i];
+        }
+
+        // Write positions to line renderer
+        _lineRenderer.SetPositions(positions);
+    }
+
+    public void ClearTrajectoryLine()
+    {
+        _lineRenderer.positionCount = 0;
+    }
+
+    private Vector2[] CalculateTrajectoryLine(Rigidbody2D rigidbody, Vector2 pos, Vector2 velocity, int steps)
+    {
+        Vector2[] results = new Vector2[steps];
+
+        float timestep = Time.fixedDeltaTime / Physics2D.velocityIterations;
+        Vector2 gravityAccel = Physics2D.gravity * rigidbody.gravityScale * timestep * timestep;
+
+        float drag = 1f - timestep * rigidbody.drag;
+        Vector2 moveStep = velocity * timestep;
+
+        for (int i = 0; i < steps; i++)
+        {
+            moveStep += gravityAccel;
+            moveStep *= drag;
+            pos += moveStep;
+            results[i] = pos;
+        }
+
+        return results;
+    }
+}
