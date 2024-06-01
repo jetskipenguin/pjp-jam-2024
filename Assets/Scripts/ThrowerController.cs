89dbc328921931e003dda023bc4df0947c956969
Collin Newman <collinnewman12@gmail.com>
2024-06-01 13:10:52 -0500

    Add player holding throwable

--- Assets/Scripts/ThrowerController.cs
+++ Assets/Scripts/ThrowerController.cs
@@ -14,22 +14,47 @@ public class ThrowerController : MonoBehaviour
     public float throwCooldown = 0.5f;
 
     private bool _canThrow = true;
+    private Collider2D _throwableCollider;
+    private GameObject _throwableInstance;
+
+    public void OnEnable()
+    {
+        initializeThrowable();
+    }
 
     public void Update()
     {
-        // Random throwable
-        GameObject throwable = throwables[Random.Range(0, throwables.Count)];
+        // If throwable is not thrown, follow player
+        if (_throwableInstance != null && _canThrow)
+        {
+            _throwableInstance.transform.position = transform.position; // TODO: add offset for player hands
+        }
 
+        // Throw the throwable
         if (Input.GetButtonDown("Throw") && _canThrow)
         {
-            // Instantiate the throwable
-            GameObject throwableInstance = Instantiate(throwable, transform.position, Quaternion.identity);
-            Rigidbody2D rb = throwableInstance.GetComponent<Rigidbody2D>();
+            _canThrow = false;
+            Rigidbody2D rb = _throwableInstance.GetComponent<Rigidbody2D>();
 
             // Throw the throwable in the direction of the players mouse
             rb.velocity = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized * throwSpeed;
+            Debug.Log("Applying Velocity to Throwable: " + rb.velocity);
+
+            StartCoroutine(TurnOnColliderOnceOutsideOfPlayer());
             StartCoroutine(ThrowCooldown());
         }
+    }
+
+    private IEnumerator TurnOnColliderOnceOutsideOfPlayer()
+    {
+        // Wait until the throwable is outside the thrower's transform to turn on collider
+        while (Vector3.Distance(_throwableInstance.transform.position, transform.position) < 0.1f)
+        {
+            yield return null;
+        }
+
+        // Once outside, enable the collider
+        _throwableCollider.enabled = true;
     }
 
     private IEnumerator ThrowCooldown()
@@ -37,5 +62,22 @@ public class ThrowerController : MonoBehaviour
         _canThrow = false;
         yield return new WaitForSeconds(throwCooldown);
         _canThrow = true;
+        initializeThrowable();
+    }
+
+    private void initializeThrowable()
+    {
+        if(!_canThrow)
+        {
+            return;
+        }
+
+        // Random throwable
+        GameObject selectedThrowable = throwables[Random.Range(0, throwables.Count)];
+
+        // Instantiate the throwable
+        _throwableInstance = Instantiate(selectedThrowable, transform.position, Quaternion.identity);
+        _throwableCollider = _throwableInstance.GetComponent<Collider2D>();
+        _throwableCollider.enabled = false;
     }
 }
