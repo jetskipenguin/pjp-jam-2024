f1295be97c0942fc17a58f1a187bff96f47f3dcc
Blob28895 <blob28895@gmail.com>
2024-06-01 16:20:54 -0500

    made sticky script and attatched it to the sticky blocks. Currently they hard stick at where they currently are attatched

---
+++ Assets/Sticky.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sticky : MonoBehaviour
+{
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject == this.gameObject)
+        {
+			return;	
+        }
+		// creates joint
+		FixedJoint2D joint = collision.gameObject.AddComponent<FixedJoint2D>();
+		Debug.Log(collision.gameObject.name);
+		Debug.Log(collision.otherCollider.gameObject.name);
+
+		// sets joint position to point of contact
+		joint.anchor = collision.contacts[0].point;
+		// conects the joint to the other object
+		joint.connectedBody = collision.contacts[0].otherCollider.transform.GetComponentInChildren<Rigidbody2D>();
+		// Stops objects from continuing to collide and creating more joints
+		//joint.enableCollision = false;
+
+	}
+}
