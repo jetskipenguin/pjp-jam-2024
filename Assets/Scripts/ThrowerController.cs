750cd88961ca5fcaefaf9a8f60c56d5af81c3269
Collin Newman <collinnewman12@gmail.com>
2024-05-31 22:37:56 -0500

    Add cooldown between throws

--- Assets/Scripts/ThrowerController.cs
+++ Assets/Scripts/ThrowerController.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class ThrowerController : MonoBehaviour
 {
     [SerializeField, Tooltip("List of objects that can be thrown")]
-    List<GameObject> throwables = new List<GameObject>();
+    public List<GameObject> throwables = new List<GameObject>();
 
     [SerializeField, Tooltip("Speed of the throw")]
-    float throwSpeed = 10f;
+    public float throwSpeed = 10f;
 
-    void Update()
+    [SerializeField, Tooltip("Length of cooldown between throws (sec)")]
+    public float throwCooldown = 0.5f;
+
+    private bool _canThrow = true;
+
+    public void Update()
     {
         // Random throwable
         GameObject throwable = throwables[Random.Range(0, throwables.Count)];
 
-        if (Input.GetButtonDown("Throw"))
+        if (Input.GetButtonDown("Throw") && _canThrow)
         {
             // Instantiate the throwable
             GameObject throwableInstance = Instantiate(throwable, transform.position, Quaternion.identity);
@@ -23,6 +28,14 @@ public class ThrowerController : MonoBehaviour
 
             // Throw the throwable in the direction of the players mouse
             rb.velocity = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized * throwSpeed;
+            StartCoroutine(ThrowCooldown());
         }
+    }
+
+    private IEnumerator ThrowCooldown()
+    {
+        _canThrow = false;
+        yield return new WaitForSeconds(throwCooldown);
+        _canThrow = true;
     }
 }
